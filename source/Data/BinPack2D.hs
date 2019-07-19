4c009299660bf5666a5f797976ef53923151a672
Topi Karvonen <topi.karvonen@gmail.com>
2019-07-19 22:03:44 +0300

    Barely tested skyline packing algorithm.

---
+++ source/Data/BinPack2D.hs
@@ -0,0 +1,136 @@
+
+module Data.BinPack2D
+	( Position(..)
+	, Size(..)
+	, Bin()
+	, emptyBin
+	, pack
+	) where
+
+import Control.Applicative
+import Control.Monad
+
+import Data.Ord
+import Data.Maybe
+import Data.Semigroup
+import Data.Monoid
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as M
+
+data Position
+	= Position
+	{ positionX :: !Word
+	, positionY :: !Word
+	} deriving (Eq, Show)
+instance Ord Position where
+	Position x0 y0 `compare` Position x1 y1 = compare y0 y1 <> compare x0 x1
+
+data Size
+	= Size
+	{ sizeWidth  :: !Word
+	, sizeHeight :: !Word
+	} deriving (Eq, Show)
+instance Ord Size where
+	Size w0 h0 `compare` Size w1 h1 = compare h0 h1 <> compare w0 w1
+
+data Bin
+	= Bin
+	{ binSize       :: !Size
+	, binSkyline    :: [Position]
+	, binGuillotine :: Map Position Size
+	} deriving (Show)
+
+emptyBin :: Size -> Bin
+emptyBin size = Bin
+	{ binSize       = size
+	, binSkyline    = [Position 0 0]
+	, binGuillotine = mempty
+	}
+
+pack :: Size -> Bin -> Maybe (Position, Bin)
+pack (Size 0  _ ) bin@Bin{..} = Nothing
+pack (Size _  0 ) bin@Bin{..} = Nothing
+pack (Size rw rh) bin@Bin{..}
+	| rw > sizeWidth binSize || rh > sizeHeight binSize = Nothing
+	| otherwise = guillotine <|> skyline where
+	-- Given starting position and skyline tail,
+	-- calculate y coordinate fitting the rectangle of size size
+	-- into the skyline tail and bin size, if such fit exists.
+	skyFit :: Position -> [Position] -> Maybe Word
+	skyFit (Position x y) ps
+		= if x+rw > sizeWidth binSize then Nothing else skyFit1 (x+rw) y ps
+	skyFit1 l py0 [] = Just py0
+	skyFit1 l py0 (Position x y : ps)
+		| x >= l    = Just py0
+		| otherwise =
+			let py1 = max py0 y
+			in if py1 + rh > sizeHeight binSize then Nothing else skyFit1 l py1 ps
+
+	-- Given position and skyline tail, calculate
+	-- the new skyline tail. The rectangle is assumed to fit.
+	skyTail :: Position -> [Position] -> [Position]
+	skyTail (Position px py) ps = Position px (py+rh) : skyTail1 (px+rw) py ps
+	skyTail1 l y0 ps0@(Position x y:ps1)
+		| x == l    = ps0
+		| x > l     = Position l y0 : ps0
+		| otherwise = skyTail1 l y ps1
+	skyTail1 l y0 [] = [Position l 0]
+
+	-- Given reversed skyline init and skyline tail,
+	-- glue them together.
+	skyGlue rps0@(Position rpx rpy : rps1) ps0@(Position px py : ps1)
+		| rpy == py = skyGlue1 rps1 $ Position rpx rpy : ps1
+		| otherwise = skyGlue1 rps0 ps0
+	skyGlue rps ps = skyGlue1 rps ps
+	skyGlue1 (p:rps) ps = skyGlue1 rps (p:ps)
+	skyGlue1 [] ps      = ps
+
+	-- Given original y, position and skyline tail, calculate wasted space.
+	-- The rectangle is assumed to fit.
+	skyWaste :: Word -> Position -> [Position] -> [(Position, Size)]
+	skyWaste oy (Position px py) ps = skyWaste1 (px+rw) py px oy ps
+	skyWaste1 _ _ _ _ [] = []
+	skyWaste1 l c x0 y0 (Position x1 y1 : ps)
+		| x1 >= l   = skyWaste2 x0 y0 (l-x0)  (c-y0) []
+		| otherwise = skyWaste2 x0 y0 (x1-x0) (c-y0) (skyWaste1 l c x1 y1 ps)
+	skyWaste2 x y w 0 cont = cont
+	skyWaste2 x y 0 h cont = cont -- Should never happen.
+	skyWaste2 x y w h cont = (Position x y, Size w h) : cont
+	
+	-- Given skyline, enumerate the options.
+	skyEnum :: [Position] -> Map Position Bin
+	skyEnum ps = skyEnum1 (sizeHeight binSize) [] ps
+	skyEnum1 _ _ [] = mempty
+	skyEnum1 y0 rps (p@(Position x1 y1) : ps)
+		| y1 >= y0  = skyEnum1 y1 (p:rps) ps
+		| otherwise = skyEnum2 x1 y1 rps ps <> skyEnum1 y1 (p:rps) ps
+	skyEnum2 x y rps ps = fromMaybe mempty $ do
+		let pos0 = Position x y
+		nh <- skyFit pos0 ps
+		let pos1 = pos0 { positionY = nh }
+		let waste = skyWaste (positionY pos0) pos1 ps
+		return $ M.singleton pos1 bin
+			{ binSkyline    = skyGlue rps $ skyTail pos1 ps
+			, binGuillotine = guilComb waste binGuillotine
+			}
+		
+
+	skyline = M.lookupMin $ skyEnum binSkyline
+
+	-- Given wasted space and guillotine, combine them to a new guillotine.
+	guilComb :: [(Position, Size)] -> Map Position Size -> Map Position Size
+	guilComb [] g0 = g0
+	guilComb ((pos,sz):ns) g0 = guilComb1 pos sz $ foldr (\(p,s) -> M.insert p s) g0 ns
+	guilComb1 pos sz g0 = fromMaybe (M.insert pos sz g0) $ do
+		(posL,szL) <- M.lookupLE pos g0
+		guard
+			$  positionY pos == positionY posL
+			&& sizeHeight sz == sizeHeight szL
+			&& positionX posL + sizeWidth szL == positionX pos
+		let nsz = Size (sizeWidth sz + sizeWidth szL) (sizeHeight sz)
+		pure $ M.insert posL nsz g0
+
+	-- TODO
+	guillotine = Nothing
+
+
